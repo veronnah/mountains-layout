44d28bb9d4839de1a839fa72788220c1065f39c7
veronnah <yoo.veroon@gmail.com>
2021-06-10 17:11:01 +0200

    header nav menu added, scrollTo active slide, animations

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+let wrapper = document.querySelector('.wrapper');
+
 let pageSlider = new Swiper('.page', {
     wrapperClass: 'page__wrapper', 
     slideClass: 'page__screen', 
@@ -26,7 +28,7 @@ let pageSlider = new Swiper('.page', {
         type: 'bullets', 
         clickable: true, 
         bulletClass: 'page__bullet', 
-        bulletActiveClass: 'page__bullet_avtive', 
+        bulletActiveClass: 'page__bullet_active', 
     },
     
     scrollbar: {
@@ -35,7 +37,42 @@ let pageSlider = new Swiper('.page', {
         draggable: true,
     },
 
+    init: false,
 
-
+    on:{
+        init: function(){
+            menuSlider();
+            wrapper.classList.add('_loaded');
+        }, 
+        slideChange: function(){
+            menuSliderRemove();
+            menuLinks[pageSlider.realIndex].classList.add('_active');
+        },
+    }
 
 });
+
+let menuLinks = document.querySelectorAll('.menu__link');
+
+function menuSlider(){
+    if(menuLinks.length > 0){
+        menuLinks[pageSlider.realIndex].classList.add('_active');
+        for(let index = 0; index < menuLinks.length; index++){
+            const menuLink = menuLinks[index];
+            menuLink.addEventListener('click', function(e){
+                menuSliderRemove();
+                pageSlider.slideTo(index, 800);
+                menuLink.classList.add('_active');
+                e.preventDefault();
+            });
+        }
+    }
+}
+
+function menuSliderRemove(){
+    let menuLinkActive = document.querySelector('.menu__link._active');
+    if(menuLinkActive){
+        menuLinkActive.classList.remove('_active');
+    }
+}
+pageSlider.init();
